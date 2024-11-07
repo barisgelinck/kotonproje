bf8cbecec3127ef5987bafbe85fd29da39e248cd
BARIS GELINCIK <baris.gelincik@gmail.com>
2024-11-07 16:20:34 +0300

    Gunluk degerlendirmedeki tasklar bitirildi. Proje olusturuldu.

---
+++ OddEvenDetector/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace OddEvenDetector
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {   
+                //Cok pratik gozukmese de dis kaynaklardan cok kod almak istemedigim icin satirlarin birbirine girmesini engellemek icin "  " yazarak cozdum.
+                Console.WriteLine("  ");
+                Console.WriteLine("Numara değerinizi girin: ");
+                Console.WriteLine("  ");
+                Console.WriteLine("Eğer çıkmak isterseniz 'exit' yazabilirsiniz.");
+                Console.WriteLine("  ");
+                string input = Console.ReadLine();
+                if (input.ToLower() == "exit")
+                {
+                    break;
+                }
+
+                //Girilen deger gercekten sayi mi diye baktigim yer
+                if (int.TryParse(input, out int numara))
+                {
+                    if (numara % 2 == 0)
+                    {
+                        Console.WriteLine("  ");
+                        Console.WriteLine("Girdiğiniz değer çift sayıdır.");
+                        Console.WriteLine("  ");
+                    }
+                    else
+                    {
+                        Console.WriteLine("  ");
+                        Console.WriteLine("Girdiğiniz değer tek sayıdır.");
+                        Console.WriteLine("  ");
+                    }
+                }
+                  else
+                {
+                    Console.WriteLine("Lütfen geçerli bir sayı değeri giriniz.");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
--- projekoton/Program.cs
+++ projekoton/Program.cs
@@ -11,159 +11,193 @@ namespace Proje
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
-            PersonManager manager = new PersonManager();
-            manager.Add(new Proje.Customer { FirstName = "Barış" });
 
-            //CustomerManager customerManager = new CustomerManager();
-            //customerManager.Add();
-            //customerManager.Update();
+//            Person[] persons = new Person[3]
+//            {
+//                    new Customer {FirstName = "Baris"},
+//                    new Student {FirstName = "Arda"},
+//                    new Person {FirstName = "Berke"}
+//            };
+//            foreach (var person in persons)
 
+//            {
+//                Console.WriteLine(person.FirstName);
+//            }
+//            Console.ReadLine();
+//        }
+//        class Person
+//        {
+//            public int Id { get; set; }
+//            public string FirstName { get; set; }
+//            public string LastName { get; set; }
+//        }
+//        class Customer : Person
+//        {
+//            public string City { get; set; }
+//        }
 
+//        class Student : Person
+//        {
+//            public string Department { get; set; }
+//        }
 
-            //    Console.ReadLine();
-            //}  
-            //Console.WriteLine("Hello World"); 
-            //Console.ReadLine();
-
-            //int number1 = 10;
-            //Console.WriteLine("number1 is {0}", number1);
-            //Console.ReadLine(); 
-
-
-            //double number5 = 10.4;
-            //decimal number6 = 10;
-            //char character = 'A';
-            //bool condition = false;
-            //byte number4 = 255;
-            //short number3 = 32767;
-            //int number1 = 2147483647;
-            //long number2 = 92232828378243;
-
-
-            //Console.WriteLine("Number1 is {0}", number1);
-            //Console.WriteLine("Number2 is {0}", number2);
-            //Console.WriteLine("Number3 is {0}", number3);
-            //Console.WriteLine("Number4 is {0}", number4);
-            //Console.WriteLine("Number5 is {0}", number5);
-            //Console.WriteLine("Character is : {0}", (int)character);
-            //Console.WriteLine((int)Days.Friday);
-            //Console.ReadLine();
-
-            //var number = 20;
-            //if (number >= 0 && number <= 100)
-            //{
-            //    Console.WriteLine("Number is between 0-100");
-            //}
-            //else if (number > 100 && number <= 200)
-            //{
-            //    Console.WriteLine("Number is between 101-200");
-            //}
-            //else if (number > 200 || number < 0)
-            //{
-            //    Console.WriteLine("Number is less than 0 or greater than 200");
-            //}
-            //string[] students = new string[3] { "Baris", "Umut", "Murat" };
-            //string[] students2 = { "Baris", "Umut", "Murat" };
-            //foreach (var student in students)
-            //{
-            //    Console.WriteLine(student);
-            //}
-            //    Console.ReadLine();
-
-            //        Array.Resize(ref students2, 4);
-            //        students2[3] = "Ali";
-
-            //        foreach (var student in students2)
-            //        {
-            //            Console.WriteLine(student);
-            //        }
-            //        string[,] regions = new string[5, 3]
-            //        {
-            //        { "İstanbul", "İzmit", "Balıkesir" },
-            //        { "Ankara", "Konya", "Kirikkale" },
-            //        { "Antalya", "Adana", "Mersin" },
-            //        { "Rize", "Trabzon", "Samsun" },
-            //        { "İzmir", "Muğla", "Manisa" }
-            //        };
+//            {
+//                PersonManager manager = new PersonManager();
+//        manager.Add(new Proje.Customer { FirstName = "Barış"});
+//    }
+//            CustomerManager customerManager = new CustomerManager();
+//    customerManager.Add();
+//            customerManager.Update();
 
 
-            //        for (int i = 0; i < regions.GetLength(0); i++)
-            //        {
-            //            for (int j = 0; j < regions.GetLength(1); j++)
-            //            {
-            //                Console.Write(regions[i, j] + " ");
-            //            }
-            //            Console.WriteLine();
 
-            //string sentence = "My full name is Baris Gelincik";
+//                Console.ReadLine();
+//            }
+//Console.WriteLine("Hello World");
+//Console.ReadLine();
 
-            //var result = sentence.Length;
-            //var result2 = sentence.Clone();
-            //sentence = "My full name is Umut Kocak";
+//int number1 = 10;
+//Console.WriteLine("number1 is {0}", number1);
+//Console.ReadLine();
 
-            //bool result3 = sentence.EndsWith("k");   
-            //bool result4 = sentence.StartsWith("My fullname is");
-            //var result5 = sentence.IndexOf("name");
-            //var result6 = sentence.IndexOf(" ");
-            //var result7 = sentence.LastIndexOf(" ");
-            //var result8 = sentence.Insert(0, "Hello, ");
-            //var result9 = sentence.Substring(3, 4);
-            //var result10 = sentence.ToLower(); 
-            //var result11 = sentence.ToUpper();
-            //var result12 = sentence.Replace("name", "adım");
-            //var result13 = sentence.Remove(2, 5);
 
-            //Console.WriteLine(result13);
-            //Console.ReadLine();
+//double number5 = 10.4;
+//decimal number6 = 10;
+//char character = 'A';
+//bool condition = false;
+//byte number4 = 255;
+//short number3 = 32767;
+//int number1 = 2147483647;
+//long number2 = 92232828378243;
 
-            //    class CustomerManager 
-            //{ 
-            //    public void Add()
-            //    {
-            //        Console.WriteLine("Customer Added.");
-            //            }
-            //deneme
-            //    public void Update()
-            //    {
-            //        Console.WriteLine("Customer Updated!");
-            //        Console.ReadLine();
-        }
 
-        interface IPerson
-        {
-            int Id { get; set; }
-            string FirstName { get; set; }
-            string LastName { get; set; }
-        }
-        class Customer : IPerson
-        {
-            public int Id { get; set; }
-            public string FirstName { get; set; }
-            public string LastName { get; set; }
+//Console.WriteLine("Number1 is {0}", number1);
+//Console.WriteLine("Number2 is {0}", number2);
+//Console.WriteLine("Number3 is {0}", number3);
+//Console.WriteLine("Number4 is {0}", number4);
+//Console.WriteLine("Number5 is {0}", number5);
+//Console.WriteLine("Character is : {0}", (int)character);
+//Console.WriteLine((int)Days.Friday);
+//Console.ReadLine();
 
-            public string Adress { get; set; }
+//var number = 20;
+//if (number >= 0 && number <= 100)
+//{
+//    Console.WriteLine("Number is between 0-100");
+//}
+//else if (number > 100 && number <= 200)
+//{
+//    Console.WriteLine("Number is between 101-200");
+//}
+//else if (number > 200 || number < 0)
+//{
+//    Console.WriteLine("Number is less than 0 or greater than 200");
+//}
+//string[] students = new string[3] { "Baris", "Umut", "Murat" };
+//string[] students2 = { "Baris", "Umut", "Murat" };
+//foreach (var student in students)
+//{
+//    Console.WriteLine(student);
+//}
+//Console.ReadLine();
 
-        }
-    }
+//Array.Resize(ref students2, 4);
+//students2[3] = "Ali";
 
-    class Student : Iperson
-    {
-        public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+//foreach (var student in students2)
+//{
+//    Console.WriteLine(student);
+//}
+//string[,] regions = new string[5, 3]
+//{
+//                    { "İstanbul", "İzmit", "Balıkesir" },
+//                    { "Ankara", "Konya", "Kirikkale" },
+//                    { "Antalya", "Adana", "Mersin" },
+//                    { "Rize", "Trabzon", "Samsun" },
+//                    { "İzmir", "Muğla", "Manisa" }
+//};
 
-        public string Departmant { get; set; }
-    }
-    class PersonManager
-    {
-        public void Add(Customer customer)
-        {
-            Console.WriteLine(customer.FirstName);
-        }
-    }
-}
+
+//for (int i = 0; i < regions.GetLength(0); i++)
+//{
+//    for (int j = 0; j < regions.GetLength(1); j++)
+//    {
+//        Console.Write(regions[i, j] + " ");
+//    }
+//    Console.WriteLine();
+
+//    string sentence = "My full name is Baris Gelincik";
+
+//    var result = sentence.Length;
+//    var result2 = sentence.Clone();
+//    sentence = "My full name is Umut Kocak";
+
+//    bool result3 = sentence.EndsWith("k");
+//    bool result4 = sentence.StartsWith("My fullname is");
+//    var result5 = sentence.IndexOf("name");
+//    var result6 = sentence.IndexOf(" ");
+//    var result7 = sentence.LastIndexOf(" ");
+//    var result8 = sentence.Insert(0, "Hello, ");
+//    var result9 = sentence.Substring(3, 4);
+//    var result10 = sentence.ToLower();
+//    var result11 = sentence.ToUpper();
+//    var result12 = sentence.Replace("name", "adım");
+//    var result13 = sentence.Remove(2, 5);
+
+//    Console.WriteLine(result13);
+//    Console.ReadLine();
+
+//                class CustomerManager
+//{
+//    public void Add()
+//    {
+//        Console.WriteLine("Customer Added.");
+//    }
+//    deneme
+//                public void Update()
+//    {
+//        Console.WriteLine("Customer Updated!");
+//        Console.ReadLine();
+//    }
+
+//    interface IPerson
+//    {
+//        int Id { get; set; }
+//        string FirstName { get; set; }
+//        string LastName { get; set; }
+//    }
+//    class Customer : IPerson
+//    {
+//        public int Id { get; set; }
+//        public string FirstName { get; set; }
+//        public string LastName { get; set; }
+
+//        public string Adress { get; set; }
+
+//    }
+//}
+
+//class Student : Iperson
+//{
+//    public int Id { get; set; }
+//    public string FirstName { get; set; }
+//    public string LastName { get; set; }
+
+//    public string Departmant { get; set; }
+//}
+//class PersonManager
+//{
+//    public void Add(Customer customer)
+//    {
+//        Console.WriteLine(customer.FirstName);
+//        Console.ReadLine();
+//    }
+//}
+//}
+
 
 
 
